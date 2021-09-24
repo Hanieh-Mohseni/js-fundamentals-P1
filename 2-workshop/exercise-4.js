81a3862f27758c6876b8fe1f5f4ea47eab770433
Hanieh-Mohseni <mohseni.hanieh@gmail.com>
2021-09-24 18:22:23 -0400

    completly done.

--- 2-workshop/exercise-4.js
+++ 2-workshop/exercise-4.js
@@ -9,7 +9,7 @@ function exercise4() {
   //
   // Edit only the code between the lines (below)
   // -----------------------------------------------------------------
-  for (let number = 1; number < 12; number++) {
+  for (let number = 0; number < 13; number++) {
     console.log(number*number);
   }
   // -----------------------------------------------------------------
