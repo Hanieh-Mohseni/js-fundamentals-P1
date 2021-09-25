3b2263aa8b8da5c399d6e71fec79d198b86a4b37
Hanieh-Mohseni <mohseni.hanieh@gmail.com>
2021-09-24 21:42:29 -0400

    Exercise6-3 completely done

--- 2-workshop/exercise-6.3.js
+++ 2-workshop/exercise-6.3.js
@@ -6,6 +6,13 @@ function exercise6_3() {
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
+  for (let number = 2; number < 51; number += 2) {
+    if (number%2==0) {
+      console.log(number);
+    }
+    
+    
+      }
   // -----------------------------------------------------------------
   // Write code between the lines (above)
   //
